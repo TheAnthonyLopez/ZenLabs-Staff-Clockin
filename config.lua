80238cadf906af0fdd316b12ee98982038648bd9
TheAnthonyLopez <anthonya46048@gmail.com>
2025-06-10 01:43:37 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {
+    AcePerm = "staffapi.clockin",
+    BypassAcePerm = "staffapi.bypass",
+    Webhook = "https://discord.com/api/webhooks/1381832289104695306/0kDfk0a3XzFGnKNmSnt47N1zvE8KBloF__62OZzUqe3cPYJddk6DW4_RIAXSaXxINQXP",
+    NotifyDuration = 8000,
+    AFKClockoutTime = 1, -- In Minutes
+    Notify = 2 -- 1 To Use Regular Chat Notifications | 2 To Use Ox_Lib Notifications
+}
